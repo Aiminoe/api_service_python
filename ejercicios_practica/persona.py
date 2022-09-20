6f7e80f12f46d3c9e8b526923c35de05c774853f
Aiminoe <106620686+Aiminoe@users.noreply.github.com>
2022-09-20 09:57:02 -0300

    Add files via upload

--- ejercicios_practica/persona.py
+++ ejercicios_practica/persona.py
@@ -53,6 +53,18 @@ def report(limit=0, offset=0):
 
     return json_result_list
 
+def dashboard():
+    x = []
+    y = []
+
+    query = db.session.query(Persona)
+    for dato in query:
+        if dato.get("id"):
+            x.append(dato)
+        if dato.get("age"):
+            y.append(dato)
+    
+    return x, y
 
 if __name__ == "__main__":
     print("Test del modulo heart.py")
